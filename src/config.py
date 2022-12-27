139f4195592e9aa382e4699e38e9b030bc337e65
XoRishu <115208147+XoRishu@users.noreply.github.com>
2022-12-27 11:47:40 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -13,24 +13,24 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 10872193  # integer value, dont use ""
-    API_HASH = "7a2c777e52479d13fb1adb29944130cf"
-    TOKEN = "5699376101:AAGtSMEy8LgqixXw2RV-Blyh7B_ZI-FCako"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 5205602399  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    API_ID =   # integer value, dont use ""
+    API_HASH = ""
+    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID =   # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "MolesteRishu"
     SUPPORT_CHAT = "ShanksxDoffy_support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001659204080
+        -
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001659204080
+        -
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    MONGO_DB_URI = "mongodb+srv://xelcius:raizel~97@cluster0.gj9j8.mongodb.net/cluster0?retryWrites=true&w=majority"
-    STRING_SESSION = "1BVtsOHQBu5YRMvuQOZWv-pFXDpfhj5_c33vo3kssCg9VhG1sneBdHC8wVbAiBg__1LbxTrJ3gYSQk-Ff6f5iyK0ZFlYz3oMwjXz-7R9goCJ5BcU-pA48T2YXaoKwmXiczE6_b14esmMqLfqQZ8gf1Ws1dFvZM77kk57jFDceiwPRqN_0YugxmQzRp6mS73OMRRFhuWBGJIURftwup7gSTYs25-8D2BUvjbp3AboBomapf7KgxeJ7e4rfQ_eoV0MXtTEjcBW39nESS0mcfG0jyO_XDNF7THuIGnZYj9SvhuWN7OHKve4NDpNLrsJpVObFCFPcStrGnuTcL3QQmFUe527P5EJeLWY="
-    ARQ_API_KEY = "HRINZL-XDAQVW-AZYEPT-ZZJZSH-ARQ"
-    SQLALCHEMY_DATABASE_URI = "postgres://ixpzecqk:5z6H-w2aq3hp4rRfMES-Ehinta3mUY37@suleiman.db.elephantsql.com/ixpzecqk"  # needed for any database modules
+    MONGO_DB_URI = ""
+    STRING_SESSION = ""
+    ARQ_API_KEY = ""
+    SQLALCHEMY_DATABASE_URI = ""  # needed for any database modules
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
